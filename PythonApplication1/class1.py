83a741fa2b933bea0c3357ad7a3c4e9326b4c6ef
benmcsausage <benjaminsaurage@yahoo.com>
2023-09-08 18:42:50 -0400

    Functions ask and fill info completed

--- PythonApplication1/class1.py
+++ PythonApplication1/class1.py
@@ -1,16 +1,23 @@
 class Workout:
-    def __init__(self, exercise,sets,reps):
-        self.exercise = exercise
-        self.sets = sets
-        self.reps = reps
-    def newfunc(self):
-        print("hello my exercise is " + self.exercise)
+    
+    def fillInfo(exercise,sets,reps,exerciseMatrix):
+        exerciseInfo = []
+        exerciseInfo.append(exercise)
+        exerciseInfo.append(sets)
+        for i in range(0,sets):
+            exerciseInfo.append(reps[i])
+        exerciseMatrix.append(exerciseInfo)
+        print(exerciseInfo)
     def ask():
-        self.exercise = input("What exercise did you do?")
-        self.sets = input("How many sets of " + self.exercise + " did you do?")
-        for i in range(0,self.sets):
-            self.sets = input("How many reps of " + self.exercise + " did you do in set number " + i + "?")
-
+        exerciseMatrix = []
+        numExercise = int(input("How many different exercises did you do in your workout?Please enter numbers only"))
+        for i in range(0,numExercise):
+            exercise = input("What exercise did you do?")
+            sets = int(input("How many sets of " + exercise + " did you do?"))
+            reps = []
+            for i in range(0,sets):
+                reps.append(int(input("How many reps of " + exercise + " did you do in set number " + str(i) + "?")))
+            Workout.fillInfo(exercise,sets,reps,exerciseMatrix)
 
 
 
