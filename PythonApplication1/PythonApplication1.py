3e7de62926133b6cc3c521793c3108761cfa3eba
benmcsausage <benjaminsaurage@yahoo.com>
2023-09-24 01:23:26 -0400

    not sure what I updated here, had a crash

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,4 +1,4 @@
-from ast import Lambda
+
 from sqlite3 import paramstyle
 import pandas as pd
 import tkinter as tk
@@ -25,6 +25,7 @@ class Progress(tk.Frame):
         # We use the switch_window_button in order to call the show_frame() method as a lambda function
 class NewWorkout(tk.Frame):
     def __init__(self, parent, controller):
+        self.x = 0
         def combinedFunc(*funcs):
             def inner_combined_func(*args, **kwargs):
                 for f in funcs:
@@ -64,7 +65,7 @@ class NewWorkout(tk.Frame):
                     labels.append(tk.Label(self, text = "How many reps did you do in set " + str(i+1) +"?"))
                     labels[i].grid(row = i+3, column = 0)                   
                     labels.append(tk.Label(self, text = "How much weight did you use in set " + str(i+1) +"?"))
-                    labels[i+1].grid(row = i++4, column = 0)
+                    labels[i+1].grid(row = i+4, column = 0)
             if(params[1]> 0):        
                 for i in range(0,params[1]*2-1):
                     entries.append(tk.Entry(self))
@@ -74,10 +75,19 @@ class NewWorkout(tk.Frame):
             sub_btn=tk.Button(self,text = 'Submit', command = lambda: [getInput1(reps,weight,entries,labels, sub_btn)])
             sub_btn.grid(row = 0, column = 3) 
         def recreateWidgets():
-            x = 0
+            def destroyWidgets2():
+                exercise_label.destroy()
+                sets_label.destroy()
+                exercise_entry.destroy()
+                sets_entry.destroy()
+                sub_btn.destroy()
+            def getInput1():
+                params.append(exercise_entry.get())
+                params.append(int(sets_entry.get()))
             Main_button=ttk.Button(self, text = "Main", command = lambda : controller.show_frame(Main))
-            Main_button.grid(row = 1, column = 0, padx = 5, pady = 5)
-            exercise_label = tk.Label(self, text = "What exercise did you do?")
+            Main_button.grid(row = 1, column = 0, padx = 5, pady = 5)           
+            exercise_label = tk.Label(self, text = "What's the next exercise you did after " + str(params))
+            self.x += 4
             exercise_label.grid(row = 2)                 
             exercise_entry = tk.Entry(self)
             exercise_entry.grid(row = 2, column  =1)        
@@ -85,13 +95,18 @@ class NewWorkout(tk.Frame):
             sets_label.grid(row = 3)
             sets_entry = tk.Entry(self)
             sets_entry.grid(row = 3, column = 1)
-            sub_btn=tk.Button(self,text = 'Submit', command = combinedFunc(getInput, destroyWidgets, askInfo))
+            sub_btn=tk.Button(self,text = 'Submit', command = combinedFunc(getInput1, destroyWidgets2, askInfo))
             sub_btn.grid(row = 4, column = 2)
+        
         self.frame = tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="NewWorkout Page")
         label.grid(row = 0)
         Main_button=ttk.Button(self, text = "Main", command = lambda : controller.show_frame(Main))
         Main_button.grid(row = 1, column = 0, padx = 5, pady = 5)
+        exnum_label = tk.Label(self, text = "How many different exercises did you do?")
+        exnum_label.grid(row = 2)
+
+        """
         exercise_label = tk.Label(self, text = "What exercise did you do?")
         exercise_label.grid(row = 2)                 
         exercise_entry = tk.Entry(self)
@@ -102,6 +117,7 @@ class NewWorkout(tk.Frame):
         sets_entry.grid(row = 3, column = 1)
         sub_btn=tk.Button(self,text = 'Submit', command = combinedFunc(getInput, destroyWidgets, askInfo))
         sub_btn.grid(row = 4, column = 2)
+        """
         
     
 
